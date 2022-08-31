a4522d7c35a736c742c06c1ad05c1511cd093eb1
Osama Gamal <97878002+Osama-Gamal@users.noreply.github.com>
2022-08-31 19:16:13 +0200

    Register with number Phone

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,9 +1,7 @@
-import 'package:firebase_project/screens/loginScreen.dart';
-import 'package:firebase_project/screens/resgisterScreen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:country_pickers/country.dart';
 import 'package:country_pickers/country_pickers.dart';
-import '../widgets/my_btn.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const String screenRoute = 'WelcomeScreen';
@@ -17,11 +15,15 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   Country _selectedDialogCountry =
       CountryPickerUtils.getCountryByPhoneCode('20');
 
+  late final _auth = FirebaseAuth.instance;
+  late String numPhone;
+  late final String countryCode;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Container(
-
         /*decoration: BoxDecoration(
           borderRadius: const BorderRadius.all(Radius.circular(5)),
           boxShadow: <BoxShadow>[
@@ -48,8 +50,9 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              const FlutterLogo(
-                size: 150,
+              Image.asset(
+                "assets/images/whatsapp.png",
+                height: 130,
               ),
               const SizedBox(
                 height: 40,
@@ -58,16 +61,26 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 child: Text(
                   "Message All",
                   style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                  ),
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.lightGreen),
                 ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "A new chat app that have a lot of new features",
+                style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black),
+                textAlign: TextAlign.center,
               ),
               const SizedBox(
                 height: 30,
               ),
-
-              welcomeBtn(
+              /* welcomeBtn(
                 btnColor: Colors.blue,
                 btnTxt: "Sign In",
                 onPressed: () {
@@ -80,45 +93,89 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 onPressed: () {
                   Navigator.pushNamed(context, RegisterScreen.screenRoute);
                 },
-              ),
+              ),*/
               Row(
                 children: [
                   Expanded(
                     flex: 1,
-                    child: Card(
-                      child: ListTile(
-                        onTap: _openCountryPickerDialog,
-                        title: _buildDialogItem(_selectedDialogCountry),
+                    child: Container(
+                      height: 55,
+                      child: Card(
+                        shape: const RoundedRectangleBorder(
+                          side: BorderSide(
+                            color: Colors.grey,
+                          ),
+                        ),
+                        child: ListTile(
+                          onTap: _openCountryPickerDialog,
+                          title: _buildDialogItem(_selectedDialogCountry),
+                        ),
                       ),
                     ),
                   ),
                   Expanded(
                     flex: 2,
                     child: TextField(
-                      keyboardType: TextInputType.emailAddress,
-                      onChanged: ((value) => {}),
+                      keyboardType: TextInputType.phone,
+                      onChanged: ((value) => {
+                            numPhone = value,
+                          }),
                       decoration: const InputDecoration(
                         filled: true,
                         fillColor: Colors.white,
                         hintText: "Enter Your Phone Number",
                         contentPadding:
-                        EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 20),
                         border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.blue, width: 1),
-                            borderRadius: BorderRadius.all(Radius.circular(10))),
-                        enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Colors.grey,
-                              width: 1,
+                            //borderRadius: BorderRadius.all(Radius.circular(10)),
                             ),
-                            borderRadius: BorderRadius.all(Radius.circular(10),),),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey, width: 1),
+
+                          /*  borderRadius:
+                                BorderRadius.all(Radius.circular(10))*/
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.grey,
+                            width: 1,
+                          ),
+                        ),
                       ),
                     ),
                   ),
                 ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print('+' + _selectedDialogCountry.phoneCode + numPhone);
+                  //_auth.signInWithPhoneNumber(_selectedDialogCountry.phoneCode+numPhone);
+                },
+                style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.zero,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10))),
+                child: Ink(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      gradient: const LinearGradient(
+                          colors: [Colors.green, Colors.lightGreen]),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Container(
+                    width: double.infinity,
+                    height: 50,
+                    alignment: Alignment.center,
+                    child: const Text(
+                      'Register',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
@@ -131,7 +188,10 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
         children: <Widget>[
           CountryPickerUtils.getDefaultFlagImage(country),
           const SizedBox(width: 8.0),
-          Text("+${country.phoneCode}",style: const TextStyle(fontSize: 15),),
+          Text(
+            "+${country.phoneCode}",
+            style: const TextStyle(fontSize: 15),
+          ),
           const SizedBox(width: 8.0),
         ],
       );
